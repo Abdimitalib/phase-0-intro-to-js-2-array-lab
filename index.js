29ef08be44d85055bd50e12076ba3e60957ef2fb
abdimutalib <abdimutalibsaid@gmail.com>
2024-09-30 15:08:10 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+console.log(Cats)
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() { 
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift(name);
+}
+
+function appendCat(name){
+    return appendCat =[...cats,"Broom"]
+}
+function prependCat(name){
+    return prependCat =['Arnold', ...cats]
+}
+function removeLastCat(){
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
